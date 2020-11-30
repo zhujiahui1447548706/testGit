d31521b75d9439ca301770a7d0b4fb1c789d6071
zjh <1392109120@qq.com>
2020-11-30 17:46:51 +0800

    项目经理第一次提交

---
+++ src/main/java/com/king/Java1304Application.java
@@ -0,0 +1,13 @@
+package com.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java1304Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java1304Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/king/Java1304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java1304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
